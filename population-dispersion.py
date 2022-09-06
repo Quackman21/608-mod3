f878cc618c28a8ed6e27d7199cba911751397834
Ivan Quackenbush <111546000+Quackman21@users.noreply.github.com>
2022-09-06 18:32:38 -0500

    Update population-dispersion.py

--- population-dispersion.py
+++ population-dispersion.py
@@ -1,1 +1,12 @@
+In [1]: import statistics
 
+In [2]: statistics.pvariance([1, 3, 4, 2, 6, 5, 3, 4, 5, 2])
+Out[2]: 2.25
+
+In [3]: statistics.pstdev([1, 3, 4, 2, 6, 5, 3, 4, 5, 2])
+Out[3]: 1.5
+
+In [4]: import math
+
+In [5]: math.sqrt(statistics.pvariance([1, 3, 4, 2, 6, 5, 3, 4, 5, 2]))
+Out[5]: 1.5
